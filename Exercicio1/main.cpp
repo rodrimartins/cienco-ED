bfc0f497839b2fcb5330de03e7cfe411b0d5708b
Rodrigo Martins <rodrimartins2005@gmail.com>
2025-02-28 22:19:45 -0300

    resolução do exercício 1

---
+++ Exercicio1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declarando as variáveis
+    float peso, altura, imc;
+
+    //entrada de dados
+    cout << "Informe o seu peso: ";
+    cin >> peso;
+    cout << "Informe a sua altura";
+    cin >> altura;
+
+    //processamento
+    imc = peso / (altura*altura);
+
+    if (imc < 20){
+        cout << "Você está abaixo do peso";
+    }
+    else if(imc >= 20 && imc < 25){
+        cout << "Você está no peso ideal";
+    }
+    else{
+        cout << "Você está acima do peso";
+    }
+
+
+    return 0;
+}
