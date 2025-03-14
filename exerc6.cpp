1e2d3328236c1f6f9075256f9608d340ec722383
Rodrigo Martins <rodrimartins2005@gmail.com>
2025-03-14 20:27:09 -0300

    Correção de todos os exercícios do material 0

---
+++ exerc6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int intervalo = 1;
+	while (intervalo < 21) {
+		cout << " QUADRADO DE " << intervalo << " = " << intervalo * intervalo << endl;
+		intervalo++;
+	}
+	return 0;
+}
---
+++ exerc7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+	for (int num = 1; num < 501; num++){
+		if (num % 5 == 0){
+			cout << num << " eh multiplo" << endl;
+		}
+
+    }
+	return 0;
+}
