773b92c7b715da26121ee7cf87b2a9c7c46aa563
Rodrigo Martins <rodrimartins2005@gmail.com>
2025-03-28 19:19:21 -0300

    organização de materiaais 0 e 1

---
+++ exemplos-material-1/exemplo1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void alo();
+
+// Programa Principal
+int main()
+{
+	alo();
+}
+
+// DEFINIÇÃO da função alo()
+void alo(void)
+{
+	cout << "Funcao simples" << endl;
+}
+
